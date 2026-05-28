1b4449c24b2af8d04261a1279620191070abd5f7
Jose David hernandez pedraza <josehernandez1156030@correo.itm.edu.co>
2026-05-27 21:09:21 -0500

    arreglo

--- Anidadas_java/Explicacion.java
+++ Anidadas_java/Explicacion.java
@@ -1,25 +1,39 @@
 
-package explicacion;
+package ciclos;
 import java.util.Scanner;
-public class Explicacion {
-    static Scanner sc= new Scanner(System.in);
-  
+public class Ciclos {
+   
     public static void main(String[] args) {
-        int v1 [][] = new int [3][3]; 
-        for(int i = 0 ; i<3; i++ ){
-            for(int j= 0 ; j<3 ; j++){
-                System.out.println("Rellene la matriz");
-                v1[i][j] = sc.nextInt();
-                
-            }
-            }
-         for(int i = 0 ; i<3; i++ ){
-             System.out.println();
-            for(int j= 0 ; j<3 ; j++){
-                System.out.print(v1[i][j]);
-            }
-            }
+           Scanner sc= new Scanner (System.in);    
+           
+        int v1 [][] = new int [3][3];
+        System.out.println("digite elementos de una matriz");
+        for (int i = 0 ; i<3 ; i++){
+            for (int j = 0 ; j<3 ; j++){
+                System.out.println("rellene la matriz");
+                v1[i][j]= sc.nextInt();
+                           
+        }
+        }
+        for (int i = 0 ; i<3 ; i++){
+            System.out.println("");
+            for (int j = 0 ; j<3 ; j++){
+            System.out.print(v1[i][j]);
+                    }
+
+    }
+        System.out.println("");
+        
+        int k=0;
+        for (int i = 0 ; i<3 ; i++){
+            k += v1[i][i];
+                        
+                    }
+        System.out.println("suma" +"="+ k);
+        
+                    }
+                        
+        
     }
     
-    
-}
+
